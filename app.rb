3bef6e0dd2cd5d83065269d64ebc5c257aa33713
Adam Dittebrand <Adam Dittebrand>
2016-10-18 17:46:10 -0400

    Got instructions and gme board to print

--- app.rb
+++ app.rb
@@ -4,18 +4,33 @@ attr_accessor :board
     @board = Array.new(3) { Array.new(3, " ") }
   end
 
+ # def printInstructions
+ #    puts "1 | 2 | 3",
+ #         "---------",
+ #         "4 | 5 | 6",
+ #         "---------",
+ #         "7 | 8 | 9"
+ #    print "\n"
+ #  end
 
  def printBoard()
     (0..2).each do |row|
-      print "       "
+      print "        "
       (0..2).each do |col|
         print @board[row][col]
         print " | " unless col == 2
       end
-end
-end
+      print "\n"
+      print "       -----------\n" unless row == 2
+    end
+    print "\n"
+  end
+
 end
 
 
-game = Board.new
-game.printBoard
+
+
+board = Board.new
+puts "\n" * 100
+board.printBoard
