9a2a42012e9637d1975f9b63fb0e99d4c095b9ed
Adam Dittebrand <Adam Dittebrand>
2016-11-11 14:35:07 -0500

    working with jtong. Game is working minus one hiccup.

--- unbeatable_ai_test.rb
+++ unbeatable_ai_test.rb
@@ -21,8 +21,25 @@ class TestUnbeatableAi < Minitest::Test
 
     def test_for_error_on_x_fourth_turn
         player = UnbeatableAi.new("o")
-        assert_equal(5, player.get_move(["o","","x","x","x","o","o","","x"]))
+        assert_equal(5, player.get_move(["o","x","o","","x","","x","o","x"]))
     end   
+    def test_for_x_block_position_5
+        player = UnbeatableAi.new("x")
+        assert_equal(5, player.get_move(["x", "", "", "o", "o", "", "", "x", ""]))
+    end
 
+    def test_for_x_position_4
+        player = UnbeatableAi.new("o")
+        assert_equal(4, player.get_move(["x", "o", "x", "o", "", "", "x", "", ""]))
+    end
 
+    def test_for_if_center_available_take_center
+        player = UnbeatableAi.new("o")
+        assert_equal(4, player.get_move(["", "", "", "", "", "", "", "", ""]))
+    end
+
+    def test_for_if_center_notavailable_take_corner
+        player = UnbeatableAi.new("x")
+        assert_equal(2, player.check_for_empty_corner(["x", "", "", "", "x", "", "", "", ""]))
+    end
 end
